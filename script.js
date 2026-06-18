8791cb58540423586015e09788155c91efe8feb4
Elecktry <ezequielcostaa015@gmail.com>
2026-06-17 21:22:33 -0300

    adiciona saldo e validação

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const lista = document.getElementById("lista");
 
+let saldo = 0;
+
 function adicionar() {
+
+    const descricaoInput =
+        document.getElementById("descricao");
+
+    const valorInput =
+        document.getElementById("valor");
+
     const descricao =
-        document.getElementById("descricao").value;
+        descricaoInput.value;
 
     const valor =
-        document.getElementById("valor").value;
+        Number(valorInput.value);
+
+    if (descricao === "" || valorInput.value === "") {
+        alert("Preencha todos os campos.");
+        return;
+    }
 
     const item = document.createElement("li");
 
     item.textContent =
-        `${descricao} - R$ ${valor}`;
+        `${descricao} - R$ ${valor.toFixed(2)}`;
 
     lista.appendChild(item);
+
+    saldo += valor;
+
+    document.getElementById("saldo").textContent =
+        `R$ ${saldo.toFixed(2)}`;
+
+    descricaoInput.value = "";
+    valorInput.value = "";
+
+    descricaoInput.focus();
 }
